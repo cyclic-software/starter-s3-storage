73d58983ae38e4b43cfd9cf5fee646e4af309195
abhinav <abhinavderapur0@gmail.com>
2022-12-19 16:33:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.post("/imageUpload", async (req, res) => {
   AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Access key ID
     secretAccesskey: process.env.AWS_SECRET_ACCESS_KEY, // Secret access key
-    region: "eu-west-1", //Region
+    region: "eu-west-1", //Region,
+    
   });
 
   const s3 = new AWS.S3();
@@ -21,6 +22,7 @@ app.post("/imageUpload", async (req, res) => {
   const fileContent = Buffer.from(req.files.uploadedFileName.data, "binary");
   // Setting up S3 upload parameters
   const params = {
+    acl: 'public-read',
     Bucket: "cyclic-nice-ruby-sea-urchin-garb-eu-west-1",
     Key: "test.jpg", // File name you want to save as  in S3
     Body: fileContent,
