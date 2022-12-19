6f54a4636d9357e442fe383d1058f4d352128a70
abhinav <abhinavderapur0@gmail.com>
2022-12-19 16:16:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,47 @@
-const express = require('express')
-const app = express()
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3()
-const bodyParser = require('body-parser');
+var express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
 
-app.use(bodyParser.json())
+app.use(fileUpload());
 
-// curl -i https://some-app.cyclic.app/myFile.txt
-app.get('*', async (req,res) => {
-  let filename = req.path.slice(1)
+var AWS = require("aws-sdk");
 
-  try {
-    let s3File = await s3.getObject({
-      Bucket: process.env.BUCKET,
-      Key: filename,
-    }).promise()
+app.post("/imageUpload", async (req, res) => {
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Access key ID
+    secretAccesskey: process.env.AWS_SECRET_ACCESS_KEY, // Secret access key
+    region: "eu-west-1", //Region
+  });
 
-    res.set('Content-type', s3File.ContentType)
-    res.send(s3File.Body.toString()).end()
-  } catch (error) {
-    if (error.code === 'NoSuchKey') {
-      console.log(`No such key ${filename}`)
-      res.sendStatus(404).end()
-    } else {
-      console.log(error)
-      res.sendStatus(500).end()
+  const s3 = new AWS.S3();
+
+  // Binary data base64
+  console.log(req.files);
+
+  const fileContent = Buffer.from(req.files.uploadedFileName.data, "binary");
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: "cyclic-nice-ruby-sea-urchin-garb-eu-west-1",
+    Key: "test.jpg", // File name you want to save as  in S3
+    Body: fileContent,
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err;
     }
-  }
-})
+    res.send({
+      response_code: 200,
+      response_message: "Success",
+      response_data: data,
+    });
+  });
+});
 
-
-// curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' https://some-app.cyclic.app/myFile.txt
-app.put('*', async (req,res) => {
-  let filename = req.path.slice(1)
-
-  console.log(typeof req.body)
-
-  await s3.putObject({
-    Body: JSON.stringify(req.body),
-    Bucket: process.env.BUCKET,
-    Key: filename,
-  }).promise()
-
-  res.set('Content-type', 'text/plain')
-  res.send('ok').end()
-})
-
-// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
-app.delete('*', async (req,res) => {
-  let filename = req.path.slice(1)
-
-  await s3.deleteObject({
-    Bucket: process.env.BUCKET,
-    Key: filename,
-  }).promise()
-
-  res.set('Content-type', 'text/plain')
-  res.send('ok').end()
-})
-
-// /////////////////////////////////////////////////////////////////////////////
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.sendStatus(404).end()
-})
-
-// /////////////////////////////////////////////////////////////////////////////
-// Start the server
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`index.js listening at http://localhost:${port}`)
-})
 
 
-
+app.listen(port, function () {
+  console.log("Example app listening on port 3000!");
+});
